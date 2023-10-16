52473a16d3304485840a7123d6848fd1fcaf8e5b
fabrice sinarinzi <sinarinzifabrice@gmail.com>
2023-10-16 12:49:22 -0400

    Ajout du prefix tw-

--- .history/resources/views/auth/confirm-password.blade_20231016123128.php
+++ .history/resources/views/auth/confirm-password.blade_20231016123128.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="tw-mb-4 tw-text-sm tw-text-gray-600">
+    <div class="mb-4 text-sm text-gray-600">
         {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
     </div>
 
@@ -10,15 +10,15 @@
         <div>
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="tw-block tw-mt-1 tw-w-full"
+            <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="tw-mt-2" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="tw-flex tw-justify-end tw-mt-4">
+        <div class="flex justify-end mt-4">
             <x-primary-button>
                 {{ __('Confirm') }}
             </x-primary-button>
