84b884c706f3f0626358303275a681135f054598
fabrice sinarinzi <sinarinzifabrice@gmail.com>
2023-10-24 14:14:50 -0400

    mis a jour package json

---
+++ .history/resources/js/app_20231024140412.js
@@ -0,0 +1,52 @@
+import './bootstrap';
+import moment from 'moment';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
+let form = document.querySelector('.needs-validation');
+form.addEventListener('submit',valider);
+
+
+function valider(e){
+    document.getElementById('error-datefin').textContent='Le champ date fin est obligatoire.';
+    document.querySelector('#datefin').setCustomValidity('');
+
+    if(form.checkValidity()==false || validerDates()==false){
+        e.preventDefault();
+    }
+    form.classList.add('was-validated');
+}
+
+
+
+function validerDates() {
+    const dateDebut = moment(document.querySelector('#datedebut').value, 'YYYY-MM-DD');
+    const dateFin = moment(document.querySelector('#datefin').value, 'YYYY-MM-DD');
+
+
+
+
+    // Vérifier si la date de fin est au moins 6 mois après la date de début
+    const sixMoisApresDebut = moment(dateDebut).add(6, 'months');
+
+
+
+    if (dateFin.isBefore(sixMoisApresDebut)) {
+        // Sélectionnez l'élément par son ID
+        var paragraphe = document.getElementById('error-datefin');
+
+        // Modifiez le message d'erreur
+        paragraphe.textContent = 'La durée de contrat est de 6 mois minimum';
+
+        document.querySelector('#datefin').setCustomValidity('sdfg');
+        return false;
+    } else {
+        document.querySelector('#datefin').setCustomValidity('');
+        return true;
+    }
+}
---
+++ .history/routes/web_20231024135641.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BienController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContratController;
+use App\Http\Controllers\LocataireController;
+use App\Http\Controllers\TypedeBienController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+// ---------------Bien-----------------------------
+Route::resource('bien', BienController::class);
+// Route::get('/', [BienController::class, 'index']);
+//--------------------fin Bien-------------------------------
+
+// ---------------typedebien-----------------------------
+Route::resource('typedebiens', TypedeBienController::class);
+//--------------------fin typedebien -------------------------
+
+// ---------------Contrat-----------------------------
+Route::resource('contrats', ContratController::class);
+//--------------------fin Contrat-------------------------------
+
+
+// ---------------locataire-----------------------------
+Route::resource('locataires', LocataireController::class);
+//--------------------fin locataire-------------------------------
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
